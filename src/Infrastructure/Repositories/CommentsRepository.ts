49725e1cf78779db8af5a077ed2196f625df46dd
Jose Jesus <jjhernandezsubs@gmail.com>
2023-03-09 19:02:36 +0100

    test comment api

--- src/Infrastructure/Repositories/CommentsRepository.ts
+++ src/Infrastructure/Repositories/CommentsRepository.ts
@@ -8,11 +8,11 @@ export default class CommentsRepository implements ICommentsRepository {
         const client = new AxiosHttpClient();
         const response = await client.get(`/search/issues?q=type:pr+commenter:${userName}+created:${month}`);
         const data = response.data;
-        console.log(response.data.items[0].comments_url.toString());
+        //console.log(response.data.items[0].comments_url.toString());
         data.items.forEach(async item => {
-            var responseComments = await client.get(item.comments_url.toString().replace("https://api.github.com", ""));
-            console.log(responseComments.data);
-            console.log("---------------------------------");
+            const responseComments = await client.get(item.comments_url.toString().replace("https://api.github.com", ""));
+            console.log(responseComments.data[0]);
+            console.log("****---------------------------------****");
         });
         const reviewComments = data.items ? data.items.filter(item => item.comments_url) : [];
         return reviewComments;
